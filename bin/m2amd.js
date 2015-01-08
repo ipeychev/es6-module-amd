7f642abe95a6fa1cdea66bdd990f16c59fee8f7e
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-01-08 11:18:04 +0100

    Add update notifier

--- bin/m2amd.js
+++ bin/m2amd.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
-var path = require('path'),
-	m2amd = require(path.join('../lib'));
+var path = require('path');
+var m2amd = require(path.join('../lib'));
+var pkg = require('../package.json');
+var updateNotifier = require('update-notifier');
+
+updateNotifier({
+	packageName: pkg.name,
+	packageVersion: pkg.version
+}).notify();
 
 m2amd();
